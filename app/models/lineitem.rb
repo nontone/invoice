95456fc640f9c84224f65aa26360ded446421de3
nontone <nontone@gmail.com>
2015-07-24 17:22:28 +0700

    updated lineitem trigger invoice when amount changed only

--- app/models/lineitem.rb
+++ app/models/lineitem.rb
@@ -2,7 +2,7 @@ class Lineitem < ActiveRecord::Base
   belongs_to :invoice
 
   before_save :calculate_amount
-  after_save { invoice.calculate_sub_total }
+  after_save { invoice.calculate_sub_total }, if: :amount_changed?
 
   def calculate_amount
     self.amount = quantity * price
